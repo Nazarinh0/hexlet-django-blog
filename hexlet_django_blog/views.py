42d95f3040d760306f5eef0171dc88a4db5e1ba4
Nazar <nazar.annahalov@yandex.ru>
2023-01-19 22:38:18 +0300

    add redirect to views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic.base import TemplateView, View
 
 
-class IndexView(TemplateView):
+class IndexView(View):
 
-    template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'World'
-        return context
+    def get(self, request):
+        return redirect(
+            reverse('article', kwargs={'tags': 'python', 'article_id': 42})
+        )
+    # template_name = 'index.html'
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['who'] = 'World'
+    #     return context
 
 
 def about(request):
