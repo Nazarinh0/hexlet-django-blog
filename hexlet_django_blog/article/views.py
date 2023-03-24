b491235540467fa9c556b8fd517067d4b5293b88
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-03-24 12:02:56 +0300

    update article/views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
 
 
-class IndexView(View):
+class ArticleView(View):
 
     def get(self, request, tags, article_id):
         return render(
@@ -10,3 +11,10 @@ class IndexView(View):
             'article/article.html',
             context={'article_id': article_id, 'tags': tags}
         )
+
+def index(request):
+    return render(
+        request,
+        'article/index.html',
+        context={'title': 'article_app'}
+    )
