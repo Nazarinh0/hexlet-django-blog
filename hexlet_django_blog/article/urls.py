fe0551d0cc702371171f4e1ccfa01aa76e8d5571
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-03-24 12:02:37 +0300

    update article/urls.py

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('<str:tags>/<int:article_id>/', views.IndexView.as_view(), name='article'),
+    path('', views.index),
+    path('<str:tags>/<int:article_id>/', views.ArticleView.as_view(), name='article'),
 ]
