e55d5c7b73d6c9fe9b8e4347fb9c2cb4d266dae4
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-03-30 15:58:11 +0300

    add comment to article/models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Article(models.Model):
-    name = models.CharField(max_length=200)
-    body = models.TextField()
+    # мы не описываем поле id, которое обычно является первичным ключом.
+    # В models.Model оно уже описано, и нам не нужно это делать дополнительно.
+    # Но мы его можем переопределить.
+    name = models.CharField(max_length=200) # название статьи
+    body = models.TextField() # тело статьи
     timestamp = models.DateTimeField(auto_now_add=True)
