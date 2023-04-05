c679ee07e5fa6914eaf1ada7ce266c89b0923170
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-05 20:19:28 +0300

    change ArticleView path

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,5 +5,5 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
-    path('<str:tags>/<int:article_id>/', views.ArticleView.as_view(), name='article'),
+    path('<int:id>/', views.ArticleView.as_view(), name='article'),
 ]
