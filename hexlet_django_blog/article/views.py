0cf124251cd03713ea726a1b4752c4c029b084cf
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-05 19:44:03 +0300

    add indexview to article/views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from hexlet_django_blog.article.models import Article
 
 
 class ArticleView(View):
@@ -12,9 +13,11 @@ class ArticleView(View):
             context={'article_id': article_id, 'tags': tags}
         )
 
-def index(request):
-    return render(
-        request,
-        'article/index.html',
-        context={'title': 'article_app'}
-    )
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'article/index.html', context={
+            'articles': articles,
+        })
