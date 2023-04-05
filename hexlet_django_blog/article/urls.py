51614e202a73938965ff943062028935f08647c4
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-05 19:43:23 +0300

    add content to article/urls.py

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-from django.views.generic.base import TemplateView
+# from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view()),
     path('<str:tags>/<int:article_id>/', views.ArticleView.as_view(), name='article'),
 ]
