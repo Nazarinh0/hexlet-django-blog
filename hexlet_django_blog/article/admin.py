fdc053bfc1a818da14f6626c6f5bc8a49ad0be63
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-05 17:00:15 +0300

    add content to admin.py

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+@admin.register(Article) # equals to commented line
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp') # Перечисляем поля, отображаемые в списке
+    search_fields = ['name', 'body']
+    list_filter = ('timestamp',) #Перечисляем поля для фильтрации
+
+
+# admin.site.register(Article, ArticleAdmin)
