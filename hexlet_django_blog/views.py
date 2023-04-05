2a4da5f7af3a8deb61a6f045894bd1ea431f896f
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-05 20:20:44 +0300

    return to standart hello world main page (temporary)

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -3,18 +3,18 @@ from django.urls import reverse
 from django.views.generic.base import TemplateView, View
 
 
-class IndexView(View):
+class IndexView(TemplateView):
 
-    def get(self, request):
-        return redirect(
-            reverse('article', kwargs={'tags': 'python', 'article_id': 42})
-        )
-    # template_name = 'index.html'
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['who'] = 'World'
-    #     return context
+    # def get(self, request):
+    #     return redirect(
+    #         reverse('article', kwargs={'tags': 'python', 'article_id': 42})
+    #     )
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
 
 
 def about(request):
