74d208fe1508fe634dea619c17256cd4755837c2
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-06 17:00:27 +0300

    update views (commentview)

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 # from django.http import HttpResponse
 from hexlet_django_blog.article.models import Article
-
+from .forms import CommentArticleForm
 
 class ArticleView(View):
 
@@ -20,3 +20,14 @@ class IndexView(View):
         return render(request, 'article/index.html', context={
             'articles': articles,
         })
+
+class CommentArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = CommentArticleForm(request.POST) # Создаем экземпляр нашей формы
+        if form.is_valid():
+            comment = Comment(
+                name - form.cleaned_data['content']
+            )
+
+        return render(request, 'comment.html', {'form': form}) # Передаем форму в контексте
