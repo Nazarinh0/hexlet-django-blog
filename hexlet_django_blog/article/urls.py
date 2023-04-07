087d70acfe5b55ca3cd699e8bbdd03c8cb040132
Nazar <nazar.annahalov@yandex.ru>
2023-04-08 00:04:28 +0300

    update urls.py (add create route)

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,6 +4,7 @@ from hexlet_django_blog.article import views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='articles'),
     path('<int:id>/', views.ArticleView.as_view(), name='article'),
+    path('create/', views.ArticleFormCreateView.as_view(), name='article_create')
 ]
