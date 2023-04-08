b343b09a21fb0be55b85aaff3c163250975e6344
Nazar <nazar.annahalov@yandex.ru>
2023-04-08 14:06:49 +0300

    add delete path to urlpatterns in article app

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,8 +4,9 @@ from hexlet_django_blog.article import views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='articles'),
+    path('', views.IndexView.as_view(), name='article_index'),
     path('<int:id>/', views.ArticleView.as_view(), name='article'),
     path('create/', views.ArticleFormCreateView.as_view(), name='article_create'),
     path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='article_update'),
+    path('<int:id>/delete/', views.ArticleFormDeleteView.as_view(), name='article_delete'),
 ]
