c92087c4ca903c6377083900f35486a32370b157
Nazar <nazar.annahalov@yandex.ru>
2023-04-08 13:23:59 +0300

    small fix in admin.py

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 
-@admin.register(Article) # equals to commented line
+
+@admin.register(Article)  # equals to commented line
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'timestamp') # Перечисляем поля, отображаемые в списке
+    list_display = ('name', 'timestamp')  # Перечисляем поля, отображаемые в списке
     search_fields = ['name', 'body']
-    list_filter = ('timestamp',) #Перечисляем поля для фильтрации
+    list_filter = ('timestamp',)  # Перечисляем поля для фильтрации
 
 
 # admin.site.register(Article, ArticleAdmin)
